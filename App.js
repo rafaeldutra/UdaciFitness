a57af88aab784aef77533ce01e4fa187f53bd867
rafael-paz <rafaelxdutra@gmail.com>
2020-03-26 21:44:07 -0300

    Adicionado calendario no History, sera preciso corrigir os erros. Fazer downgrade do projeto apra testar.

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default class App extends React.Component {
         return ( 
           <Provider store={createStore(reducer)}>
             <View style={{flex: 1}}>
+              <View style={{height: 20}}></View>
               <History/>
             </View>
           </Provider>
